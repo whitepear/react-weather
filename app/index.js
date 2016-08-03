bd7e5fb03660c0458136680444f8153bf6e81f73
John Doe <webdevnorth@gmail.com>
2016-08-03 23:53:02 +0100

    index files routed and styled for react

--- app/index.js
+++ app/index.js
@@ -1,15 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var HelloWorld = React.createClass({
-	render: function () {
-		return (
-			<h1>Hello React!</h1>
-		)
-	}
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-	<HelloWorld />,
-	document.getElementById('app')
+	routes,
+	document.getElementById('app')	
 );
