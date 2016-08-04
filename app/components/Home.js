2534f459d23e7076e444382e7a8635bea766a4d1
John Doe <webdevnorth@gmail.com>
2016-08-04 14:28:02 +0100

    completed layout and styling of Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,18 +1,48 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
-var Home = React.createClass({
-	render: function () {
-		var homeStyles = {
-			background: 'pink',
-			height: '90%'
-		};
+function Home (props) {
+	var inputContainer = {
+		backgroundImage: 'url(./app/images/symphony.png)',
+		height: '90%',
+		'textAlign': 'center'
+	};
 
-		return (
-			<div style={homeStyles}>
+	var inputHeader = {
+		margin: '0 auto',		
+		paddingTop: '300',
+		color: '#fff',
+		fontSize: '45',
+		marginBottom: '30',		
+		fontFamily: "'Lato', sans-serif"
+	};
 
+	var inputControls = {
+		width: '80%',
+		maxWidth: '350',
+		margin: '0 auto'
+	};
+
+	var submitButton = {
+		marginTop: '20',
+		backgroundColor: '#079F75',
+		color: '#fff',
+		padding: '10'
+	};
+
+	return (
+		<div style={inputContainer}>
+			<h1 style={inputHeader}>Enter a City and State</h1>
+			<div style={inputControls}>
+				<input type="text" placeholder="St. George, Utah" className="form-control" />
+				<button className="btn" type="button" style={submitButton}>Get Weather</button>				
 			</div>
-		)
-	}
-});
+		</div>
+	)	
+}
+
+// Home.propTypes = {
+
+// };
 
 module.exports = Home;
