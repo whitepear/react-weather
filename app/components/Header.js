2d188ffaabc8da79050d4f3f54f4230ff2f01fa5
John Doe <webdevnorth@gmail.com>
2016-08-05 16:56:36 +0100

    refactored Header to be stateless functional

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,53 +1,59 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
-var Header = React.createClass({
-	render: function () {
-		var mainNav = {
-			height: '8%',			
-			minHeight: 67,
-			background: 'linear-gradient(#FC833F, #F35E0A)'	
-		};
+function Header (props) {
+	
+	var mainNav = {
+		height: '8%',			
+		minHeight: 67,
+		background: 'linear-gradient(#FC833F, #F35E0A)'	
+	};
 
-		var navLogo = {
-			margin: 0,
-			display: 'inline-block',
-			color: '#fff',
-			fontFamily: "'Lato', sans-serif",
-			marginLeft: 20,
-			position: 'relative',
-			top: 20						
-		};
+	var navLogo = {
+		margin: 0,
+		display: 'inline-block',
+		color: '#fff',
+		fontFamily: "'Lato', sans-serif",
+		marginLeft: 20,
+		position: 'relative',
+		top: 20						
+	};
 
-		var textInput = {
-			position: 'relative',
-			top: 20,
-			float: 'right',
-			marginRight: 10,
-			height: 34,
-			borderRadius: 3,
-			border: 'none',
-			padding: 5,
-			paddingLeft: 10,
-			width: 220
-		};
+	var textInput = {
+		position: 'relative',
+		top: 20,
+		float: 'right',
+		marginRight: 10,
+		height: 34,
+		borderRadius: 3,
+		border: 'none',
+		padding: 5,
+		paddingLeft: 10,
+		width: 220
+	};
 
-		var headerButton = {
-			backgroundColor: '#079F75',
-			color: '#fff',
-			position: 'relative',
-			top: 20,
-			float: 'right',
-			marginRight: 15
-		};
+	var headerButton = {
+		backgroundColor: '#079F75',
+		color: '#fff',
+		position: 'relative',
+		top: 20,
+		float: 'right',
+		marginRight: 15
+	};
 
-		return (
-			<div style={mainNav}>
-				<h2 style={navLogo}>OpenWeather</h2>				
-				<button type="button" className="btn hidden-xs" style={headerButton}>Get Weather</button>
-				<input type="text" className="hidden-xs" placeholder="St. George, Utah" style={textInput} />
-			</div>
-		)
-	}
-});
+	return (
+		<div style={mainNav}>
+			<h2 style={navLogo}>OpenWeather</h2>				
+			<button type="button" className="btn hidden-xs" style={headerButton}>Get Weather</button>
+			<input type="text" className="hidden-xs" placeholder="St. George, Utah" style={textInput} />
+		</div>
+	)
+}
+
+Header.propTypes = {
+	onUpdate: PropTypes.func.isRequired,
+	onSubmit: PropTypes.func.isRequired
+}
+
 
 module.exports = Header;
