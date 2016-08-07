56f9df72ec39d80a6ea8a9c50b3eb3b641bd6c80
John Doe <webdevnorth@gmail.com>
2016-08-07 21:34:28 +0100

    created DetailContainer, included in routing

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,12 +7,14 @@ var hashHistory = ReactRouter.hashHistory;
 var Main = require('../containers/MainContainer.js');
 var HomeContainer = require('../containers/HomeContainer.js');
 var ForecastContainer = require('../containers/ForecastContainer.js');
+var DetailContainer = require('../containers/DetailContainer.js');
 
 var routes = (
 	<Router history={hashHistory}>
 		<Route path='/' component={Main}>
 			<IndexRoute component={HomeContainer} />
 			<Route path='forecast/:location' component={ForecastContainer} />
+			<Route path='detail/:day' component={DetailContainer} />
 		</Route>
 	</Router>
 );
---
+++ app/containers/DetailContainer.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var DetailContainer = React.createClass({
+	render: function () {
+		return (
+			<div>Detail container!</div>
+		)
+	}
+});
+
+module.exports = DetailContainer;
