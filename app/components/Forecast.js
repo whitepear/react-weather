bc404e9c8e55b28fc343196bb3a16a8e5cd8ee6a
John Doe <webdevnorth@gmail.com>
2016-08-07 20:43:47 +0100

    styled forecast.js

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,17 +1,45 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var getDate = require('../utils/getDate.js');
 var key = 0;
 
 function Forecast (props) {
-	var iBigger = {
-		fontSize: 55
-	};	
-
+	
 	function uniqueKey () {
 		key++;
 		console.log(key);
 		return key;
 	}
+
+	var font = {
+		fontFamily: "'Lato', sans-serif"
+	};
+
+	var locationHeader = {		
+		marginTop: 30,
+		fontSize: 80
+	};
+
+	var subHeader = {
+		marginTop: 15,
+		marginBottom: 40,
+		fontSize: 30
+	};
+
+	var colStyle = {
+		marginTop: 30
+	};
+
+	var iBigger = {
+		fontSize: 205,
+		cursor: 'pointer'		
+	};
+
+	var dateText = {
+		position: 'relative',
+		bottom: 30,
+		fontSize: 30
+	};
 	
 	if (props.isLoading) {
 		return (
@@ -19,16 +47,17 @@ function Forecast (props) {
 		)
 	} else {
 		return (
-			<div className="container">
+			<div className="container" style={font}>
 				<div className="row text-center">
-					<div className="col-xs-12">{props.location}</div>
-					<div className="col-xs-12">Select a day</div>					
+					<div className="col-xs-12" style={locationHeader}>{props.location}</div>
+					<div className="col-xs-12" style={subHeader}>Select a day</div>					
 				</div>
 				<div className="row">					
-					{props.weatherForecast.list.map(function (day) {						
+					{props.weatherForecast.list.map(function (day, index) {						
 						var statusCode = day.weather[0].id;
-						return <div key={uniqueKey()} className="col-xs-12 col-sm-4 col-md-3">
-									   <i className={'owf ' + 'owf-' + statusCode} style={iBigger}></i>{day.weather[0].description}
+						return <div key={uniqueKey()} className="col-xs-12 col-sm-4 col-md-3 text-center" style={colStyle}>
+									   <i className={'owf ' + 'owf-' + statusCode} style={iBigger}></i>
+									   <div style={dateText}>{getDate(index)}</div>
 									 </div>	
 					})}					
 				</div>
