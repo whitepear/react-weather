2db563848128db17b36c0512e5ef2dac7811968f
John Doe <webdevnorth@gmail.com>
2017-01-04 22:44:01 +0000

    DetailContainer will redirect to index when visited directly in address bar

--- client/containers/DetailContainer.js
+++ client/containers/DetailContainer.js
@@ -2,10 +2,26 @@ var React = require('react');
 var Detail = require('../components/Detail.js');
 
 var DetailContainer = React.createClass({
-	render: function () {		
-		return (
-			<Detail id={this.props.location.state.id} forecast={this.props.location.state.forecast} />
-		);
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	componentWillMount: function() {		
+		if (this.props.location.state === null) {
+			this.context.router.push('/');
+		}
+	},
+	render: function () {
+		if (this.props.location.state) {
+			return (
+				<Detail id={this.props.location.state.id} forecast={this.props.location.state.forecast} />
+			);
+		}	else {
+			// this exists in case of users visiting /detail directly in the address bar, as
+			// componentWillMount won't redirect before some initial render
+			return (
+				<div></div>
+			)
+		}			
 	}
 });
 
