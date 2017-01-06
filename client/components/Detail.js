8479ce8ab9f9f8f1b92239aebee30dfc5fae30ed
John Doe <webdevnorth@gmail.com>
2017-01-06 16:05:47 +0000

    application is responsively styled for mobile devices

--- client/components/Detail.js
+++ client/components/Detail.js
@@ -8,7 +8,7 @@ function Detail(props) {
 	
 	return (
 		<div className="container text-center">
-			<div className="location-header">{props.forecast.city.name + ', ' + props.forecast.city.country}</div>
+			<div className="detail-location-header">{props.forecast.city.name + ', ' + props.forecast.city.country}</div>
 			<i className={'i-detail owf ' + 'owf-' + statusCode}></i>
 			<div className="date-text">{getDate(props.id)}</div>
 			<div className="description-text">
